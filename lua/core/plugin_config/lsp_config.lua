927a3fdb00827fb3aeab916db026cddc1b86dd10
CJBSoftware <carman.babin@cjbsoftware.com>
2023-09-19 17:27:37 -0500

    added ensure installed

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,6 +1,13 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
 
+require("mason-lspconfig").setup {
+  ensure_installed = {
+    "lua_ls",
+    "rust_analyzer",
+  },
+}
+
 require("lspconfig").lua_ls.setup {}
 require("lspconfig").rust_analyzer.setup {}
 require('lspconfig').svelte.setup{}
--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,4 +1,27 @@
 require'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+    "c_sharp",
+    "diff",
+    "gitcommit",
+    "bash",
+    "comment",
+    "css",
+    "html",
+    "javascript",
+    "jsdoc",
+    "jsonc",
+    "lua",
+    "markdown",
+    "regex",
+    "scss",
+    "toml",
+    "typescript",
+    "yaml",
+    "svelte",
+    "vim",
+    "toml",
+    "rust"
+  },
   sync_install = false,
   auto_install = true,
   highlight = {
