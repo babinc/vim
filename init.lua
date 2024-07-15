450ac5c72e769cd491b5ca8a02b65cdae13aab01
CJBSoftware <carman.babin@cjbsoftware.com>
2024-07-14 22:30:16 -0500

    only ask for C# dll file if trying to debug a C# program

--- init.lua
+++ init.lua
@@ -1,84 +1,3 @@
-local function get_dll_path()
-  -- Define the project directory and locate the .csproj file
-  local project_dir = vim.fn.getcwd()
-  local csproj_file = vim.fn.glob(project_dir .. '/*.csproj')
-  local csproj_dir = vim.fn.fnamemodify(csproj_file, ':h')
-  local dll_path_file = csproj_dir .. '/dll_path.txt'
-
-  -- Function to save the DLL path to the file
-  local function save_dll_path(dll_path)
-    local file = io.open(dll_path_file, "w")
-    if file then
-      file:write(dll_path)
-      file:close()
-    else
-      print("Error: Unable to save DLL path.")
-    end
-  end
-
-  -- Function to read the DLL path from the file
-  local function read_dll_path()
-    local file = io.open(dll_path_file, "r")
-    if file then
-      local dll_path = file:read("*all")
-      file:close()
-      return dll_path
-    else
-      return nil
-    end
-  end
-
-  -- Check if the DLL path file exists
-  local dll_path = read_dll_path()
-  if dll_path and vim.fn.filereadable(dll_path) == 1 then
-    return dll_path
-  else
-    -- Prompt the user for the DLL path
-    dll_path = vim.fn.input('Path to dll: ', csproj_dir .. '\\bin\\Debug', 'file')
-    save_dll_path(dll_path)
-    return dll_path
-  end
-end
-
--- Function to build the C# project
-_G.build_project = function()
-  local build_command = 'dotnet build'
-  vim.fn.jobstart(build_command, {
-    cwd = vim.fn.getcwd(),
-    on_exit = function(_, code)
-      if code == 0 then
-        print('Build successful.')
-      else
-        print('Build failed.')
-      end
-    end,
-  })
-end
-
--- Function to build and start debugging
-_G.build_and_debug = function()
-  -- Modify the build command based on your project setup
-  local build_command = 'dotnet build'
-  vim.fn.jobstart(build_command, {
-    cwd = vim.fn.getcwd(),
-    on_exit = function(_, code)
-      if code == 0 then
-        -- Build successful, now start debugging
-        require('dap').run({
-          type = 'coreclr',
-          name = 'launch - netcoredbg',
-          request = 'launch',
-          program = get_dll_path(),
-          cwd = vim.fn.getcwd(),
-          stopOnEntry = false,
-        })
-      else
-        print('Build failed. Unable to start debugging.')
-      end
-    end,
-  })
-end
-
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
@@ -143,7 +62,14 @@ dap.configurations.cs = {
     type = 'coreclr',
     name = 'launch - netcoredbg',
     request = 'launch',
-    program = get_dll_path(),
+    program = function()
+      local dll_path = get_dll_path()
+      if dll_path then
+        return dll_path
+      else
+        error("Unable to find .csproj file or DLL path.")
+      end
+    end,
     cwd = vim.fn.getcwd(),
     stopOnEntry = false,
   },
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,143 @@
-if true then return { } end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+_G.get_dll_path = function()
+  -- -- Define the project directory and locate the .csproj file
+  -- local project_dir = vim.fn.getcwd()
+  -- local csproj_file = vim.fn.glob(project_dir .. '/*.csproj')
+  -- local csproj_dir = vim.fn.fnamemodify(csproj_file, ':h')
+  -- local dll_path_file = csproj_dir .. '/dll_path.txt'
+  --
+  -- -- Function to save the DLL path to the file
+  -- local function save_dll_path(dll_path)
+  --   local file = io.open(dll_path_file, "w")
+  --   if file then
+  --     file:write(dll_path)
+  --     file:close()
+  --   else
+  --     print("Error: Unable to save DLL path.")
+  --   end
+  -- end
+  --
+  -- -- Function to read the DLL path from the file
+  -- local function read_dll_path()
+  --   local file = io.open(dll_path_file, "r")
+  --   if file then
+  --     local dll_path = file:read("*all")
+  --     file:close()
+  --     return dll_path
+  --   else
+  --     return nil
+  --   end
+  -- end
+  --
+  -- -- Check if the DLL path file exists
+  -- local dll_path = read_dll_path()
+  -- if dll_path and vim.fn.filereadable(dll_path) == 1 then
+  --   return dll_path
+  -- else
+  --   -- Prompt the user for the DLL path
+  --   dll_path = vim.fn.input('Path to dll: ', csproj_dir .. '\\bin\\Debug', 'file')
+  --   save_dll_path(dll_path)
+  --   return dll_path
+  -- end
+
+
+  -- Define the project directory and locate the .csproj file
+  local project_dir = vim.fn.getcwd()
+  local csproj_file = vim.fn.glob(project_dir .. '/*.csproj')
+
+  -- Check if a .csproj file exists
+  if csproj_file == '' then
+    print("No .csproj file found in the project directory.")
+    return nil
+  end
+
+  local csproj_dir = vim.fn.fnamemodify(csproj_file, ':h')
+  local dll_path_file = csproj_dir .. '/dll_path.txt'
+
+  -- Function to save the DLL path to the file
+  local function save_dll_path(dll_path)
+    local file = io.open(dll_path_file, "w")
+    if file then
+      file:write(dll_path)
+      file:close()
+    else
+      print("Error: Unable to save DLL path.")
+    end
+  end
+
+  -- Function to read the DLL path from the file
+  local function read_dll_path()
+    local file = io.open(dll_path_file, "r")
+    if file then
+      local dll_path = file:read("*all")
+      file:close()
+      return dll_path
+    else
+      return nil
+    end
+  end
+
+  -- Check if the DLL path file exists
+  local dll_path = read_dll_path()
+  if dll_path and vim.fn.filereadable(dll_path) == 1 then
+    return dll_path
+  else
+    -- Prompt the user for the DLL path
+    dll_path = vim.fn.input('Path to dll: ', csproj_dir .. '\\bin\\Debug', 'file')
+    save_dll_path(dll_path)
+    return dll_path
+  end
+
+end
+
+-- Function to build the C# project
+_G.build_project = function()
+  local build_command = 'dotnet build'
+  vim.fn.jobstart(build_command, {
+    cwd = vim.fn.getcwd(),
+    on_exit = function(_, code)
+      if code == 0 then
+        print('Build successful.')
+      else
+        print('Build failed.')
+      end
+    end,
+  })
+end
+
+-- Function to build and start debugging
+_G.build_and_debug = function()
+  -- Modify the build command based on your project setup
+  local build_command = 'dotnet build'
+  vim.fn.jobstart(build_command, {
+    cwd = vim.fn.getcwd(),
+    on_exit = function(_, code)
+      if code == 0 then
+        -- Build successful, now start debugging
+        require('dap').run({
+          type = 'coreclr',
+          name = 'launch - netcoredbg',
+          request = 'launch',
+          program = function()
+            local dll_path = get_dll_path()
+            if dll_path then
+              return dll_path
+            else
+              error("Unable to find .csproj file or DLL path.")
+            end
+          end,
+          cwd = vim.fn.getcwd(),
+          stopOnEntry = false,
+        })
+      else
+        print('Build failed. Unable to start debugging.')
+      end
+    end,
+  })
+end
+
+if true then return {
+
+} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
