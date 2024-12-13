3adda7a3c2ecaf134517bf4a86f4a67a15ffc9ca
Carman Babin <cbabin@slb.com>
2024-12-13 15:18:57 -0600

    fix WSL issues

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -9,17 +9,21 @@ vim.keymap.set('n', "<Leader>fF", function() require("telescope.builtin").find_f
 vim.keymap.set('n', "<Leader>ft", function() require("telescope.builtin").colorscheme { enable_preview = true, ignore_builtins = true } end, { desc = "Find themes" })
 vim.keymap.set('n', "<Leader>f'", function() require("telescope.builtin").marks() end, { desc = "Find marks" })
 vim.keymap.set('n', "<Leader>f<CR>", function() require("telescope.builtin").resume() end, { desc = "Resume previous search" })
+-- vim.keymap.set('n', '<leader>fs', function()
+-- 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+--   use({
+--     "rebelot/heirline.nvim",
+--     -- You can optionally lazy-load heirline on UiEnter
+--     -- to make sure all required plugins and colorschemes are loaded before setup
+--     -- event = "UiEnter",
+--     -- config = function()
+--     --   require("heirline").setup({})
+--     -- end
+--   })
+-- end, { desc = "Search With Grep" })
+
 vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-  use({
-    "rebelot/heirline.nvim",
-    -- You can optionally lazy-load heirline on UiEnter
-    -- to make sure all required plugins and colorschemes are loaded before setup
-    -- event = "UiEnter",
-    -- config = function()
-    --   require("heirline").setup({})
-    -- end
-  })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, { desc = "Search With Grep" })
 
 -- Mapping for finding files in ~/.config/nvim
